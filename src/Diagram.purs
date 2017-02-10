5379b52da40227188085efbf9918e6936161a950
Garrett <graffitisouls@gmail.com>
2017-02-09 20:03:36 -0600

    A relatively reasonable pass at turning Diagram into a Halogen component.

--- src/Diagram.purs
+++ src/Diagram.purs
@@ -1,17 +1,35 @@
 module App.Diagram where
 
-import App.Element as E
-import Data.Foldable
 import Prelude
+import Data.Foldable
 import Data.Array (insertBy)
-import Graphics.Canvas.Free (fillRect, setFillStyle)
+import Data.Maybe (Maybe(Just,Nothing))
+import Control.Monad.Aff (Aff)
+import Control.Monad.Eff (Eff)
+
+import Halogen
+import Halogen.HTML.Indexed as H
+import Halogen.HTML.Events.Indexed as HE
+import Halogen.HTML.Properties.Indexed as HP
+
+import Graphics.Canvas (CANVAS, getCanvasElementById, getContext2D, setCanvasDimensions, Context2D)
+import Graphics.Canvas.Free (fillRect, setFillStyle, runGraphics)
+
+import App.Element as E
 
 type State = { paused :: Boolean
+             , ctx :: Maybe Context2D
              , color :: { r :: Int, g :: Int, b :: Int }
              , statics :: Array E.Drawable
              , elements :: Array E.Drawable 
              }
-                  
+             
+data Query a 
+  = TogglePlay a
+  | Initialize a
+  | Tick a
+
+
 advanceFrame :: State -> State
 advanceFrame st = st {elements = map updateOne st.elements } where
   updateOne (E.Drawable d) = d.updated unit
@@ -19,13 +37,53 @@ advanceFrame st = st {elements = map updateOne st.elements } where
 insertElement :: State -> E.Drawable -> State
 insertElement st dr = 
   st {elements = insertBy (comparing (\(E.Drawable d) -> d.layer)) dr st.elements}
+  
+  
+drawGraphics st = let 
+  drawOne (E.Drawable d) = d.drawn in
+  case st.ctx of
+    Just ctx -> runGraphics ctx $ do
+                  setFillStyle $ E.colorToStr st.color
+                  fillRect {x: 0.0, y:0.0, w: 1000.0, h: 1000.0}
+                  traverse_ drawOne st.statics
+                  traverse_ drawOne st.elements
+    Nothing  -> pure unit
 
-render st = 
-  let drawOne (E.Drawable d) = d.drawn in
-  do 
-    setFillStyle $ E.colorToStr st.color
-    fillRect {x: 0.0, y:0.0, w: 1000.0, h: 1000.0}
-    traverse_ drawOne st.statics
-    traverse_ drawOne st.elements
+diaComp :: forall eff. Component State Query (Aff (canvas :: CANVAS | eff))
+diaComp = lifecycleComponent
+  { render: render
+  , eval: eval
+  , initializer: Just (action Initialize)
+  , finalizer: Nothing
+  } where
+  render :: State -> ComponentHTML Query
+  render st =
+    H.div_
+      [ H.canvas [ HP.id_ "canvas" ]
+      ]
+      
+  eval :: Query ~> ComponentDSL State Query (Aff (canvas :: CANVAS | eff))
+  eval (TogglePlay next) = do
+    pause <- gets _.paused
+    modify (\st -> st {paused = not pause})
+    pure next
+  
+  eval (Tick next) = do
+    pause <- gets _.paused
+    if pause
+      then pure next
+      else do
+        modify advanceFrame
+        st <- get
+        fromEff $ drawGraphics st
+        pure next
     
-  --do stuff to render the HTML element here!
+  eval (Initialize next) = do
+    cv <- fromEff $ getCanvasElementById "canvas"
+    case cv of
+      Nothing -> pure next
+      Just canvas -> do
+          fromEff $ setCanvasDimensions {width: 800.0, height: 800.0} canvas
+          context <- fromEff $ getContext2D canvas
+          modify (\state -> state {ctx = Just context})
+          pure next
--- src/Main.purs
+++ src/Main.purs
@@ -1,28 +1,18 @@
 module Main where
 
-import App.Diagram (advanceFrame, render)
+import Prelude
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class
+
+import Halogen as H
+import Halogen.Util (runHalogenAff, awaitBody)
+
 import Example.IntermissionA
+import App.Diagram
 
-import Prelude (bind, ($))
-import Data.Maybe (Maybe(Just, Nothing))
-
-import Control.Monad.Eff.Console (log)
 import Control.Monad.Eff.Timer (setInterval)
-import Control.Monad.ST (modifySTRef, newSTRef, readSTRef)
 
-import Graphics.Canvas (getCanvasElementById, getContext2D, setCanvasDimensions)
-import Graphics.Canvas.Free (runGraphics)
-
-main = do
-  cv <- getCanvasElementById "canvas"
-  case cv of
-    Nothing -> log "no canvas element found?"
-    Just canvas -> do
-      setCanvasDimensions {width: 800.0, height: 800.0} canvas
-      context <- getContext2D canvas
-      ref <- newSTRef interA
-      setInterval 16 do 
-        modifySTRef ref \el -> advanceFrame el
-        l <- readSTRef ref
-        runGraphics context $ render l
-      log "ok"
+main = runHalogenAff do
+  body <- awaitBody
+  driver <- H.runUI diaComp interA body
+  liftEff $ setInterval 16 $ runHalogenAff (driver (H.action Tick)) --what.
