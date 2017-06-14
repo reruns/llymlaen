a0fbd275bfe5249127ea73ebc8dcdf473183dfe6
Garrett <graffitisouls@gmail.com>
2017-06-14 15:36:53 -0500

    Fixed borders not appearing for circles and rectangles.

--- client/src/Element-Presets.purs
+++ client/src/Element-Presets.purs
@@ -12,8 +12,8 @@ circBase :: Int -> Point -> Element
 circBase t p = Element { layer: 0
                , keys: [Keyframe { time: t
                           , props: [ Enabled true
-                                   , Bordered false
                                    , Color $ RGB {r:128,g:128,b:128}
+                                   , Bordered false
                                    , Position p
                                    , Opacity 100
                                    , Circle 50
@@ -25,8 +25,8 @@ rectBase :: Int -> Point -> Element
 rectBase t p = Element { layer: 0
                , keys: [Keyframe { time: t
                           , props: [ Enabled true
-                                   , Bordered false
                                    , Color $ RGB{r:128,g:128,b:128}
+                                   , Bordered false
                                    , Position p
                                    , Opacity 100
                                    , Angle 0
