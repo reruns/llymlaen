245efe55c55b94796170d59daaff5529748e61bd
Garrett <graffitisouls@gmail.com>
2017-06-15 03:36:59 -0500

    Element editor now has descriptive text.

--- client/src/Components/ElementEditor.purs
+++ client/src/Components/ElementEditor.purs
@@ -89,19 +89,21 @@ component =
     locked <- H.gets _.locked
     pure (reply locked)
 
-renderProp i (Enabled b)   = [checkBox [HP.title "enabled"]  b (FormChange i <<< Enabled)]
-renderProp i (Bordered b)  = [checkBox [HP.title "bordered"] b (FormChange i <<< Bordered)]
+renderProp i (Enabled b)   = [HH.div_ [checkBox [HP.title "enabled"]  b (FormChange i <<< Enabled), HH.text "Visible"]]
+renderProp i (Bordered b)  = [HH.div_ [checkBox [HP.title "bordered"] b (FormChange i <<< Bordered), HH.text "Border"]]
 renderProp i (Color c)     = 
-  map (\{v,h} -> slider [] 0 255 v (FormChange i <<< Color <<< h)) 
-  [{v:getR c,h:setR c}, {v:getG c,h:setG c}, {v:getB c,h:setB c}]
+  map (\{v,h,l} -> HH.div_ [ slider [] 0 255 v (FormChange i <<< Color <<< h),  HH.text l]) 
+  [{v:getR c,h:setR c,l:"Red"}, {v:getG c,h:setG c,l:"Green"}, {v:getB c,h:setB c,l:"Blue"}]
 renderProp i (Position p)  = 
-  map (\{v,h} -> number [] v (FormChange i <<< Position <<< h)) 
-  [{v:getX p,h:setX p}, {v: getY p, h: setY p}]
-renderProp i (Opacity o)   = [slider [HP.title "opacity"] 0 100 o (FormChange i <<< Opacity)]
-renderProp i (Angle a)     = [slider [HP.title "angle"] 0 360 a (FormChange i <<< Angle)]
-renderProp i (Circle r)    = [ number [] r (FormChange i <<< Circle) ]
-renderProp i (Rect w h)    = [ number [] w (FormChange i <<< (flip Rect) h)
-                             , number [] h (FormChange i <<< Rect w) ]
-renderProp i (Donut r1 r2) = [ number [] r1 (FormChange i <<< (flip Donut) r2)
-                             , number [] r2 (FormChange i <<< Donut r1) ]
+  map (\{v,h,l} -> HH.div_ [ number [] v (FormChange i <<< Position <<< h), HH.text l]) 
+  [{v:getX p,h:setX p,l:"X"}, {v: getY p, h: setY p,l:"Y"}]
+renderProp i (Opacity o)   = [HH.div_ [slider [HP.title "opacity"] 0 100 o (FormChange i <<< Opacity), HH.text "Opacity"]]
+renderProp i (Angle a)     = [HH.div_ [slider [HP.title "angle"] 0 360 a (FormChange i <<< Angle), HH.text "Rotation"]]
+renderProp i (Circle r)    = [HH.div_ [ number [] r (FormChange i <<< Circle), HH.text "Radius" ]]
+renderProp i (Rect w h)    = [ HH.div_ [ number [] w (FormChange i <<< (flip Rect) h), HH.text "Width"]
+                             , HH.div_ [ number [] h (FormChange i <<< Rect w), HH.text "Height" ]
+                             ]
+renderProp i (Donut r1 r2) = [ HH.div_ [ number [] r1 (FormChange i <<< (flip Donut) r2), HH.text "Inner Radius"]
+                             , HH.div_ [ number [] r2 (FormChange i <<< Donut r1), HH.text "Outer Radius" ]
+                             ]
     
